fcc9f858bff4a74d7d07c4a3c0497565c730f1db
josh161001 <estrella161610@gmail.com>
2023-11-24 02:36:02 -0600

    modified

--- src/components/layout/admin/ModalCrearEvento.js
+++ src/components/layout/admin/ModalCrearEvento.js
@@ -192,15 +192,14 @@ const ModalCrearEvento = ({ isOpen, onClose }) => {
   return (
     <>
       {isOpen && (
-        <div className="fixed mt-0 top-0 right-0 bottom-0 left-0 flex justify-center items-center w-full bg-gray-800 bg-opacity-80  z-50">
-          <div className="p-4 bg-gray-800 rounded-lg max-w-sm md:max-w-2xl lg:max-w-4xl">
+        <div className="fixed mt-0 top-0 right-0 bottom-0  left-0 flex justify-center items-center w-full bg-gray-800 bg-opacity-80  z-50">
+          <div className="p-4 bg-gray-800 rounded-lg max-w-xs md:max-w-3xl lg:max-w-4xl">
             <div className="flex border-b border-gray-700">
-              <h2 className="text-2xl text-gray-900 dark:text-white mb-4">
-                Agregar Evento
-              </h2>
+              <h2 className="text-sm text-white mb-3">Agregar Evento</h2>
+
               <button
                 onClick={onClose}
-                className="text-gray-400 bg-transparent hover:text-gray-500 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover-bg-gray-600 dark:hover-text-white"
+                className=" bg-transparent rounded-lg text-sm p-1 ml-auto text-white inline-flex items-center hover-bg-gray-600 hover-text-white"
               >
                 <svg
                   aria-hidden="true"
@@ -221,62 +220,62 @@ const ModalCrearEvento = ({ isOpen, onClose }) => {
             <form className="bg-gray-800 " onSubmit={guardarUsuario}>
               <div className="grid grid-cols-2 lg:grid-cols-3 md:grid-cols-2 gap-2 ">
                 <div className="lg:mb-2">
-                  <label className="block mb-2 pt-1 lg:pt-4  text-sm font-medium text-white">
+                  <label className="block mb-2 pt-1 lg:pt-4  text-xs font-medium text-white">
                     Nombre del evento
                   </label>
                   <input
                     onChange={eventoState}
                     name="name"
                     type="text"
-                    className="border text-sm rounded-lg  block w-full pl-2.5 p-2 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+                    className="border text-xs rounded-lg  block w-full pl-2.5 p-1.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
                     placeholder="Evento de..."
                   />
                 </div>
 
                 <div className="lg:mb-2">
-                  <label className="block mb-2 pt-1 lg:pt-4  text-sm font-medium text-white">
+                  <label className="block mb-2 pt-1 lg:pt-4  text-xs font-medium text-white">
                     Detalles del evento
                   </label>
                   <input
                     onChange={eventoState}
                     name="detalles"
                     type="text"
-                    className="bg-gray-50  border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-2.5 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="border  text-xs rounded-lg  block w-full pl-2.5 p-1.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
                     placeholder="Agregar tambien servicios con (...)"
                   />
                 </div>
 
                 <div className="lg:mb-2">
-                  <label className="block mb-2 pt-1 lg:pt-4  text-sm font-medium text-white">
+                  <label className="block mb-2 lg:pt-4 pt-1  text-xs font-medium text-gray-900 text-white">
                     Lugar del evento
                   </label>
                   <input
                     onChange={eventoState}
                     name="lugar"
                     type="text"
-                    className="bg-gray-50  border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-2.5 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="border text-xs rounded-lg  block w-full pl-2.5 p-1.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
                     placeholder="Evento de..."
                   />
                 </div>
                 <div className="lg:mb-2">
-                  <label className="block mb-2 pt-1 lg:pt-4  text-sm font-medium text-white">
+                  <label className="block mb-2 pt-5 lg:pt-4 text-xs font-medium text-white">
                     Cupo del evento
                   </label>
                   <input
                     onChange={eventoState}
                     name="cupo"
                     type="number"
-                    className="bg-gray-50  border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-2.5 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="border text-xs rounded-lg  block w-full pl-2.5 p-1.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
                     placeholder="Evento de..."
                   />
                 </div>
                 <div className="lg:mb-2">
-                  <label className="block mb-2 pt-1 lg:pt-4  text-sm font-medium text-white">
+                  <label className="block mb-2 pt-1 lg:pt-4 text-xs font-medium text-white">
                     Categoria del evento
                   </label>
                   <select
                     name="categoryId"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="border text-xs rounded-lg  block w-full p-1.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
                     onChange={eventoState}
                     value={evento.categoryId}
                   >
@@ -292,22 +291,21 @@ const ModalCrearEvento = ({ isOpen, onClose }) => {
                 </div>
 
                 <div className="lg:mb-2">
-                  <label className="block mb-2 lg:pt-4 pt-1  text-sm font-medium text-gray-900 dark:text-white">
+                  <label className="block mb-2 pt-1 lg:pt-4 text-xs font-medium text-white">
                     Imagen del evento
                   </label>
                   <input
-                    className="w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                    className="w-full text-xs   rounded-lg cursor-pointer text-gray-400 focus:outline-none bg-gray-700 border-gray-600 placeholder-gray-400"
                     type="file"
                     name="imagen"
                     onChange={eventoState}
                   />
-                  <div className=" text-sm text-gray-500 pt-1 dark:text-gray-300">
+                  <div className="mt-1 text-xs text-gray-300">
                     Solo se permiten imágenes tipo jpg, png, jpeg, gif
                   </div>
                 </div>
                 <div className="lg:mb-2">
-                  {" "}
-                  <label className="block ml-2 lg:mt-4  text-sm font-medium text-white">
+                  <label className="block ml-2  text-xs font-medium text-white">
                     Estatus del evento
                   </label>
                   <input
@@ -332,8 +330,7 @@ const ModalCrearEvento = ({ isOpen, onClose }) => {
                   </label>
                 </div>
                 <div className="lg:mb-2">
-                  {" "}
-                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white pt-2">
+                  <label className="block mb-2  text-xs font-medium text-white">
                     Fecha del evento
                   </label>
                   <DatePicker
@@ -344,16 +341,13 @@ const ModalCrearEvento = ({ isOpen, onClose }) => {
                     timeIntervals={15}
                     timeCaption="Hora"
                     dateFormat="dd/MM/yyyy h:mm aa"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-2.5 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-2.5 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   />
                 </div>
               </div>
               <div>
                 <div>
-                  <label
-                    htmlFor="message"
-                    className="block mb-2 pt-1 text-sm font-medium text-gray-900 dark:text-white"
-                  >
+                  <label className="block mb-4 pt-2 text-xs font-medium text-white">
                     Descripcion del evento
                   </label>
                   <trix-editor
